bc8c88d187fa58568556346b62f329d34632a9a1
attm1122 <attm1122@gmail.com>
2018-01-28 15:48:00 +1100

    Swift 3, first project
    Swift 3, Xcode 9.
    
    small app using optionals.

--- Swift Cat years/ViewController.swift
+++ Swift Cat years/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var ageTxtfield: UITextField!
+    
+    @IBOutlet var ageLabel: UILabel!
+    
+    @IBAction func submitPressed(_ sender: AnyObject) {
+        
+        let ageInCatYears = Int(ageTxtfield.text!)! * 7
+        
+        ageLabel.text = String(ageInCatYears) 
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
